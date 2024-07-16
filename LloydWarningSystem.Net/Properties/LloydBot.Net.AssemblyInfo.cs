e84b3e8bb482e2fb5322d8dd62b3f074753482da
Sombody101 <evansheyborne@gmail.com>
2024-07-16 17:31:35 -0600

    csproj fix for github actions

--- LloydWarningSystem.Net/Properties/LloydBot.Net.AssemblyInfo.cs
+++ LloydWarningSystem.Net/Properties/LloydBot.Net.AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-/* Generated on Tuesday, July 16, 2024 12:59:17 PM */
-[assembly: AssemblyVersion("2024.07.16.1259")]
-[assembly: AssemblyFileVersion("2024.07.16.1259")]
+/* Generated on Tuesday, July 16, 2024 05:31:04 PM */
+[assembly: AssemblyVersion("2024.07.16.1731")]
+[assembly: AssemblyFileVersion("2024.07.16.1731")]
