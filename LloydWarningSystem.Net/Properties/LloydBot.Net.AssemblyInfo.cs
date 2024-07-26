cd7d74c37f0d0e008312423a3b3a26411fe53f6f
Sombody101 <evansheyborne@gmail.com>
2024-07-25 23:52:55 -0600

    DB Migrations

--- LloydWarningSystem.Net/Properties/LloydBot.Net.AssemblyInfo.cs
+++ LloydWarningSystem.Net/Properties/LloydBot.Net.AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-/* Generated on Thursday, July 25, 2024 11:36:29 PM */
-[assembly: AssemblyVersion("2024.07.25.2336")]
-[assembly: AssemblyFileVersion("2024.07.25.2336")]
-[assembly: AssemblyInformationalVersion("2024.07.25.2336 RC1.4")]
+/* Generated on Thursday, July 25, 2024 11:52:28 PM */
+[assembly: AssemblyVersion("2024.07.25.2352")]
+[assembly: AssemblyFileVersion("2024.07.25.2352")]
+[assembly: AssemblyInformationalVersion("2024.07.25.2352 RC1.4")]
