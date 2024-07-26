00f2e6dc95060afac37afb4f9991cca1dbf933d9
Sombody101 <evansheyborne@gmail.com>
2024-07-25 23:48:47 -0600

    Project updates

---
+++ LloydWarningSystem.Net/Context/DictionaryAliasConverter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Newtonsoft.Json;
+
+namespace LloydWarningSystem.Net.Context;
+
+public class DictionaryValueConverter : ValueConverter<Dictionary<string, string>, string>
+{
+    public DictionaryValueConverter() : base(
+        v => JsonConvert.SerializeObject(v),
+        v => JsonConvert.DeserializeObject<Dictionary<string, string>>(v))
+    {
+    }
+}
