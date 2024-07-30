8e7489b7ff196f4ff933fe22b7e8ed3ade31e5f5
Sombody101 <evansheyborne@gmail.com>
2024-07-30 15:22:56 -0600

    DB Migrations

---
+++ LloydWarningSystem.Net/Migrations/20240729051307_StopFuckingBreakingPls.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LloydWarningSystem.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class StopFuckingBreakingPls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "reaction_emoji",
+                table: "users",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "reaction_emoji",
+                table: "users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
---
+++ LloydWarningSystem.Net/Migrations/20240730183518_AfkStatusEntity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LloydWarningSystem.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class AfkStatusEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AfkStatusEntity",
+                columns: table => new
+                {
+                    id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    afk_message = table.Column<string>(type: "TEXT", maxLength: 70, nullable: true),
+                    user_id = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    afk_epoch = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AfkStatusEntity", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_AfkStatusEntity_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AfkStatusEntity_user_id",
+                table: "AfkStatusEntity",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AfkStatusEntity");
+        }
+    }
+}
